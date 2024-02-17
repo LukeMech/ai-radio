8865391eae2b05e8d7a43bf966a5380921e4946f
LukeMech <lukaszjajkommz@gmail.com>
2024-02-17 23:04:11 +0100

    yea

--- api/index.py
+++ api/index.py
@@ -1,8 +1,7 @@
 from sanic import Sanic
 from sanic.response import json
-app = Sanic()
- 
- 
+app = Sanic('Hello!')
+
 @app.route('/')
 @app.route('/<path:path>')
 async def index(request, path=""):
