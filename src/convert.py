b6984be22111cec83bbac8628fea753c1ccfd091
LukeMech <lukaszjajkommz@gmail.com>
2024-02-18 23:03:53 +0100

    Add socket for authenticating, some playback changes

--- src/convert.py
+++ src/convert.py
@@ -1,19 +1,19 @@
 from pydub import AudioSegment
 import os
 
-def m4a_to_mp3(m4a_file, mp3_file):
+def m4a_to_mp3(in_file, out_file):
     # Load the M4A file
-    audio = AudioSegment.from_file(m4a_file, format="m4a")
+    audio = AudioSegment.from_file(in_file, format="mp3")
 
     # Export the audio to MP3 format
-    audio.export(mp3_file, format="mp3")
+    audio.export(out_file, format="wav")
 
 if __name__ == "__main__":
     # Specify the input M4A file and output MP3 file
-    input_m4a_file = "test.m4a"
-    output_mp3_file = "test.mp3"
+    in_file = "test.mp3"
+    out_file = "test.wav"
 
     # Convert M4A to MP3
-    m4a_to_mp3(input_m4a_file, output_mp3_file)
+    m4a_to_mp3(in_file, out_file)
 
     print("Conversion complete.")
--- src/index.py
+++ src/index.py
@@ -1,49 +1,108 @@
-from flask import Flask, Response, render_template
-from multiprocessing import Process
-import subprocess
+from flask import Flask, Response, render_template, request
+from flask_socketio import SocketIO
+import subprocess, time, threading
 
 app = Flask(__name__)
-p = None  # Define p globally
-
-@app.route('/')
-def index():
-    response = render_template('index.html')
-    response = add_no_cache_headers(response)
-    return response
+socketio = SocketIO(app)
+radio = {
+    "ffmpeg_processes": {},
+    "active_connections": {},
+    "time": 0
+}
+WAV_FILE_PATH = 'test.wav'
 
 def add_no_cache_headers(response):
-    response = Response(response)
     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'  # Prevent caching by the browser
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '0'
     return response
 
-def start_audio_stream():
-    global p
-    while True:
-        command = [
-            'ffmpeg',
-            '-re',             # Read data from input at native frame rate
-            '-i', 'test.mp3',  # Path to the file
-            '-f', 'mp3',       # Output format MP3
-            '-'
-        ]
-        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=10**8)
-        p.wait()  # Wait for the subprocess to finish before restarting
-
+@app.route('/')
+def index():
+    response = add_no_cache_headers(Response(render_template('index.html')))
+    return response
+    
 @app.route('/listen')
 def listen():
-    global p
-    if p is None or p.poll() is not None:  # If the subprocess has not been started or has finished
-        start_audio_stream()  # Start the audio streaming process
+    global radio
+    session_id = request.remote_addr
+    if session_id not in radio["active_connections"]:
+        return "Not authorized", 403  # Return forbidden status if user is not connected via WebSocket
+    
+    elif session_id in radio["ffmpeg_processes"]:
+        print(f"Terminating ffmpeg process for IP {session_id}...")
+        radio["ffmpeg_processes"][session_id].terminate()
+        del radio["ffmpeg_processes"][session_id]
 
-    if p and p.stdout:  # Check if p and its stdout attribute are not None
-        return Response(iter(lambda: p.stdout.read(1024), b''), mimetype='audio/mpeg')
-    else:
-        return Response(b'', mimetype='audio/mpeg')
+    print(f"Starting ffmpeg proccess for ip {str(session_id)}...")
+    ffmpeg_process = start_ffmpeg_process(radio["time"])
+    radio["ffmpeg_processes"][session_id] = ffmpeg_process
+
+    time.sleep(1)
+
+    return add_no_cache_headers(Response(generate_audio(ffmpeg_process), mimetype='audio/mpeg'))
+
+@socketio.on('connect')
+def handle_connect():
+    global radio
+    session_id = request.remote_addr
+    print(f"Client connected: {session_id}")
+    radio["active_connections"][session_id] = True
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    session_id = request.remote_addr
+    print(f"Client disconnected: {session_id}")
+    global radio
+    radio["active_connections"].pop(session_id, None)  # Remove user from active connections
+    if session_id in radio["ffmpeg_processes"]:
+        print(f"Terminating ffmpeg process for IP {session_id}...")
+        radio["ffmpeg_processes"][session_id].terminate()
+        del radio["ffmpeg_processes"][session_id]
+
+def start_ffmpeg_process(start_time):
+    # Rozpocznij proces ffmpeg
+    command = [
+        'ffmpeg',
+        '-re',                      # Read data from input at native frame rate
+        '-stream_loop', '-1',       # Loop indefinitely
+        '-ss', str(start_time),     # Start from given time
+        '-i', WAV_FILE_PATH,        # Input file
+        '-f', 'mp3',                # Output format MP3,
+        '-'
+    ]
+    return subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=10**8)
+
+def generate_audio(ffmpeg_process):
+    while True:
+        data = ffmpeg_process.stdout.read(1024)
+        if not data:
+            break
+        else: 
+            yield data
+
+def get_audio_duration(file_path):
+    # Run ffprobe to get audio duration
+    result = subprocess.run(['ffprobe', '-v', 'error', '-show_entries',
+                             'format=duration', '-of', 'default=noprint_wrappers=1:nokey=1', file_path],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    duration = float(result.stdout)
+    return duration
+
+def count_time():
+    duration = get_audio_duration(WAV_FILE_PATH)
+    global radio
+    while True:
+        # Increment time by 1 second
+        radio["time"] += 0.1
+        if radio["time"] > duration:
+            # If time exceeds duration, reset to 0
+            radio["time"] = 0
+        time.sleep(0.1)
 
 if __name__ == '__main__':
-    audio_process = Process(target=start_audio_stream)
-    audio_process.start()
+    time_thread = threading.Thread(target=count_time)
+    time_thread.daemon = True
+    time_thread.start()
 
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=8000)
