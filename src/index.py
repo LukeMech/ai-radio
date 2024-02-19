df57ca017ee032b2278bc4c1b87791f83215750e
LukeMech <lukaszjajkommz@gmail.com>
2024-02-19 02:01:33 +0100

    hmm

--- src/index.py
+++ src/index.py
@@ -25,10 +25,10 @@ def index():
 @app.route('/listen')
 def listen():
     global radio
-    session_id = request.headers.get('Device-ID')  # Użyj nagłówka Device-ID jako identyfikatora sesji
+    session_id = request.headers.get('devid')  # Użyj nagłówka devid jako identyfikatora sesji
 
     if not session_id:
-        return "Missing Device-ID header", 400  # Zwróć błąd 400 Bad Request, jeśli brakuje nagłówka Device-ID
+        return "Missing devid header", 400  # Zwróć błąd 400 Bad Request, jeśli brakuje nagłówka devid
     if session_id not in radio["active_connections"]:
         return "Not connected to websocket, not authorized", 403  # Return forbidden status if user is not connected via WebSocket
     
@@ -50,17 +50,17 @@ def listen():
 @socketio.on('connect')
 def handle_connect():
     global radio
-    session_id = str(request.headers.get('Device-ID'))  # Użyj nagłówka Device-ID jako identyfikatora sesji
+    session_id = str(request.headers.get('devid'))  # Użyj nagłówka devid jako identyfikatora sesji
     if not session_id:
-        app.logger.error("No Device-ID header provided for the connection.")
-        raise ConnectionRefusedError('Missing Device-ID header')  # Odrzuć połączenie socketu
+        app.logger.error("No devid header provided for the connection.")
+        raise ConnectionRefusedError('Missing devid header')  # Odrzuć połączenie socketu
     
     app.logger.info("Client connected with session id: " + session_id)
     radio["active_connections"][session_id] = True
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    session_id = str(request.headers.get('Device-ID'))  # Użyj nagłówka Device-ID jako identyfikatora sesji
+    session_id = str(request.headers.get('devid'))  # Użyj nagłówka devid jako identyfikatora sesji
     app.logger.info("Client disconnected with session id: " + session_id)
     global radio
     radio["active_connections"].pop(session_id, None)  # Remove user from active connections
