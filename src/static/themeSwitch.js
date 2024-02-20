be735f805dd18c803d379dc378460d1fa3a4c844
LukeMech <lukaszjajkommz@gmail.com>
2024-02-20 19:11:12 +0100

    ha

--- src/static/themeSwitch.js
+++ src/static/themeSwitch.js
@@ -4,20 +4,20 @@ function detectColorScheme(){
 
     //local storage is used to override OS theme settings
     if(localStorage.getItem("theme")){
-        if(localStorage.getItem("theme") == "dark"){
-            var theme = "dark";
+        if(localStorage.getItem("theme") == "light"){
+            var theme = "light";
         }
     } else if(!window.matchMedia) {
         //matchMedia method not supported
         return false;
-    } else if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        //OS theme setting detected as dark
-        var theme = "dark";
+    } else if(window.matchMedia("(prefers-color-scheme: light)").matches) {
+        //OS theme setting detected as light
+        var theme = "light";
     }
 
-    //dark theme preferred, set document with a `data-theme` attribute
-    if (theme=="dark") {
-         document.documentElement.setAttribute("data-theme", "dark");
+    //light theme preferred, set document with a `data-theme` attribute
+    if (theme=="light") {
+         document.documentElement.setAttribute("data-theme", "light");
     }
 }
 detectColorScheme();
