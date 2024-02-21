430f7b6f5748d2b0feb027a16ec747b36dad509f
LukeMech <lukaszjajkommz@gmail.com>
2024-02-21 19:43:25 +0100

    Always preloaded waiting thunbnail

--- src/static/playerInfo.js
+++ src/static/playerInfo.js
@@ -1,21 +1,24 @@
 document.querySelector('body').addEventListener('mainLoaded', () => {
     const currentlyPlayingTitle = document.getElementById('currently-playing-title');
     const currentlyPlayingAuthor = document.getElementById('currently-playing-author');
+    const currentlyPlayingImageWait = document.getElementById('currently-playing-image-wait');
     const currentlyPlayingImage = document.getElementById('currently-playing-image');
     currentlyPlayingTitle.innerHTML = '...'
     currentlyPlayingAuthor.innerHTML = '...'
-    currentlyPlayingImage.src = '/static/radio.ico' 
 
     socket.on('trackChange', args => {
         currentlyPlayingTitle.innerHTML = args.title
         currentlyPlayingAuthor.innerHTML = args.author
         currentlyPlayingImage.src = args.thunbnail
         if(!args.thunbnail) currentlyPlayingImage.src = '/static/radio.ico' 
+        currentlyPlayingImageWait.classList.add('hidden')
+        currentlyPlayingImage.classList.remove('hidden')
         // args.duration, args.time (how many already played)
     });
     socket.on('disconnect', () => {
         currentlyPlayingTitle.innerHTML = '...'
         currentlyPlayingAuthor.innerHTML = '...'
-        currentlyPlayingImage.src = '/static/radio.ico' 
+        currentlyPlayingImage.classList.add('hidden')
+        currentlyPlayingImageWait.classList.remove('hidden')
     });
 })
