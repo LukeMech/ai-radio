5636bfb268d3b85e8feae8d40255059ce0ba1d38
LukeMech <lukaszjajkommz@gmail.com>
2024-02-23 00:30:49 +0100

    Add alpha info dialog

--- src/web/scripts/language.js
+++ src/web/scripts/language.js
@@ -1,9 +1,11 @@
 var currentLanguage = '', languageStrings = '';
 
+const warn = document.getElementById('WARN');
 async function fetchStrings(language) {
     currentLanguage = language
     languageStrings = await fetch(`languages/${language}.json`).then(response => response.json())
     document.querySelector('body').dispatchEvent(new Event('languagesLoaded'));
+    warn.innerHTML = languageStrings.currentWarning
 }
 
 fetch(`languages/supported.json`).then(response => response.json()).then(supportedLanguages => {
