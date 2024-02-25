21d55c3f39bab56c3c462802c76d8c33000834e8
LukeMech <lukaszjajkommz@gmail.com>
2024-02-25 17:39:52 +0100

    Bruh

--- src/helpers/youtube.py
+++ src/helpers/youtube.py
@@ -3,14 +3,12 @@ from youtube_title_parse import get_artist_title
 
 # Somehow using aria2 prevents github actions from stopping...? 
 # So, let's use it even though its slower (slows at the dwnld end)
-def downloadWavFromUrl(url, callback, i, country):
+def downloadWavFromUrl(url, callback, i):
     ERR=None; title=None; artist=None; fpath=None; ext=None; thunb=None
     try:
         ext = 'wav'
         fpath = 'tmp/' + ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
 
-        country = 'PL'
-        
         ydl_opts = {
             'format': 'bestaudio/best',
             'download_options': '-N 16',
@@ -37,12 +35,9 @@ def downloadWavFromUrl(url, callback, i, country):
             'writethumbnail': True,  # Write thumbnail
             'merge_output_format': ext,  # Merge into .wav file
         }
-        if(country): ydl_opts['geo_bypass_country'] = country
-
-        print(country, True)
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            url = "https://www.youtube.com/watch?v=EAexp0w3H3c" # TESTING
+            url = "https://www.youtube.com/watch?v=Bmliz34QCZc" # TESTING
             info_dict = ydl.extract_info(url, download=True)
         
         filename = fpath + '.' + ext
